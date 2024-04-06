51e51cbdbf66799fcce7cdc60ce8ec72fd231545
Mykyta Zemlianyi <zemlianoyne@gmail.com>
2024-04-06 14:10:12 +0200

    add Brasket Splitter constructor, getters and setters of config and AbsolutePathToConfig, and createDeliveryMap method and unit test for createDeliveryMap, setConfig

--- src/main/java/com/ocado/basket/BasketSplitter/BasketSplitter.java
+++ src/main/java/com/ocado/basket/BasketSplitter/BasketSplitter.java
@@ -1,18 +1,84 @@
 package com.ocado.basket.BasketSplitter;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 
 public class BasketSplitter {
-	public static final String absolutePathToConfigFile = null;
-	
+	private String absolutePathToConfigFile;
+	private Map<String, List<String>> config;
+
 	public BasketSplitter(String absolutePathToConfigFile) {
-		
+		this.absolutePathToConfigFile = absolutePathToConfigFile;
+		this.config = setConfig();
+	}
+
+	public Map<String, List<String>> getConfig() {
+		return config;
+	}
+
+	public String getAbsolutePathToConfigFile() {
+		return absolutePathToConfigFile;
+	}
+
+	public void setAbsolutePathToConfigFile(String absolutePathToConfigFile) {
+		this.absolutePathToConfigFile = absolutePathToConfigFile;
 	}
 
 	public Map<String, List<String>> split(List<String> items) {
-		
+
 		return null;
 	}
 
+	public Map<String, List<String>> createDeliveryMap(List<String> items) {
+		Map<String, List<String>> deliveryMap = new HashMap<>();
+
+		for (String item : items) {
+			List<String> deliveryMethods = getConfig().get(item);
+
+			if (deliveryMethods != null) {
+				for (String deliveryMethod : deliveryMethods) {
+					if (!deliveryMap.containsKey(deliveryMethod)) {
+						// Создаем новый список для данного способа доставки
+						deliveryMap.put(deliveryMethod, new ArrayList<>());
+					}
+					// Добавляем товар в список для данного способа доставки
+					deliveryMap.get(deliveryMethod).add(item);
+				}
+			}
+		}
+
+		return deliveryMap;
+	}
+
+	public Map<String, List<String>> setConfig() {
+
+		Map<String, List<String>> config = new HashMap<>();
+
+		try (JsonReader reader = Json.createReader(new FileReader(getAbsolutePathToConfigFile()))) {
+			JsonObject jsonObject = reader.readObject();
+
+			for (String key : jsonObject.keySet()) {
+				JsonArray jsonArray = jsonObject.getJsonArray(key);
+				List<String> values = new ArrayList<>();
+
+				for (int i = 0; i < jsonArray.size(); i++) {
+					values.add(jsonArray.getString(i));
+				}
+
+				config.put(key, values);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return config;
+	}
 }
--- src/test/java/com/ocado/basket/BasketSplitter/BasketSplitterTest.java
+++ src/test/java/com/ocado/basket/BasketSplitter/BasketSplitterTest.java
@@ -4,18 +4,72 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BasketSplitterTest {
-	
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-private BasketSplitter BasketSplitter;
-	
-@BeforeEach
-void setUp() {
-	BasketSplitter = new BasketSplitter( com.ocado.basket.BasketSplitter.BasketSplitter.absolutePathToConfigFile);
-}
+public class BasketSplitterTest {
+
+	private BasketSplitter basketSplitter;
+
+	@BeforeEach
+	void setUp() {
+		String absolutePathToConfigFile = "C:\\workspace\\BasketSplitter\\src\\main\\resources\\config.json";
+		this.basketSplitter = new BasketSplitter(absolutePathToConfigFile);
+
+	}
 
 	@Test
-	void testAdd() {
-        fail();
-    }
+	public void testSetConfig() {
+
+		Map<String, List<String>> config = basketSplitter.setConfig();
+
+		assertTrue(!config.isEmpty());
+
+		assertEquals(100, config.size());
+
+		assertTrue(config.containsKey("Cookies Oatmeal Raisin"));
+		assertEquals(2, config.get("Cookies Oatmeal Raisin").size());
+
+		assertTrue(config.containsKey("Cheese Cloth"));
+		assertEquals(5, config.get("Cheese Cloth").size());
+
+	}
+
+	@Test
+	public void testCreateDeliveryMap() {
+		List<String> itemList = new ArrayList<>();
+		itemList.add("Cocoa Butter");
+		itemList.add("Tart - Raisin And Pecan");
+		itemList.add("Table Cloth 54x72 White");
+		itemList.add("Flower - Daisies");
+		itemList.add("Fond - Chocolate");
+		itemList.add("Cookies - Englishbay Wht");
+
+		Map<String, List<String>> deliveryMap = basketSplitter.createDeliveryMap(itemList);
+
+		assertTrue(deliveryMap.containsKey("Next day shipping"));
+
+		boolean containsCocoaButter = false;
+		for (List<String> items : deliveryMap.values()) {
+			if (items.contains("Cocoa Butter")) {
+				containsCocoaButter = true;
+				break;
+			}
+		}
+		assertTrue(containsCocoaButter);
+
+		assertTrue(deliveryMap.containsKey("Mailbox delivery"));
+
+		containsCocoaButter = false;
+		for (List<String> items : deliveryMap.values()) {
+			if (items.contains("Fond - Chocolate")) {
+				containsCocoaButter = true;
+				break;
+			}
+		}
+		assertTrue(containsCocoaButter);
+
+	}
+
 }
